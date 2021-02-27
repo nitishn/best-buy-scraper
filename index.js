472b7d2c584b70d41d955ed2d114d2d15ee13cab
Nitish Narala <nitish.narala@kps3.com>
2021-02-27 12:51:28 -0800

    Turn entire app into a looping structure

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 // Web scraper using Puppeteer
 // Process each product with a source that can be different logic per product
 const puppeteer = require('puppeteer');
-let productsConfig = [
-    {
-      'product'   : 'rtx 3070',
-      'url'       : 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442',
-    },
-    {
-      'product' : 'rtx 3080',
-      'url'     : 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
-    }
-];
+const productsConfig = require('./productsConfig');
 
 const addToCartSelctor = '.btn-lg.add-to-cart-button';
 
@@ -28,20 +19,30 @@ const addToCartSelctor = '.btn-lg.add-to-cart-button';
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
 
-  productsConfig.foreach((product) => {
-    console.log(product);
-  });
+  let counter = 0;
 
-  // Do the actual page vist
-  await page.goto('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
+  while (true) {
 
-  // Grab the add to cart button and check it's text
-  let buttons = await page.$$(addToCartSelctor);
-  if(buttons.length) {
-    // Only grab one button in case more than one match
-    buttons = buttons.shift();
-    let inStockText = await buttons.evaluate(el => el.textContent )
-    console.log(inStockText);
+    if (counter == productsConfig.length) {
+      counter = 0;
+    }
+
+    console.log('Checking product: ' + productsConfig[counter % productsConfig.length].product);
+
+    // Do the actual page vist
+    await page.goto(productsConfig[counter % productsConfig.length].url);
+
+    // Grab the add to cart button and check it's text
+    let buttons = await page.$$(addToCartSelctor);
+    if(buttons.length) {
+      // Only grab one button in case more than one match
+      buttons = buttons.shift();
+      let inStockText = await buttons.evaluate(el => el.textContent )
+      console.log(inStockText);
+    } else {
+      console.log('Could not find cart button');
+    }
+    counter++;
   }
-  await browser.close();
+
 })();
---
+++ productsConfig.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+      'product'   : 'NVIDIA GeForce RTX 3070 8GB',
+      'url'       : 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442',
+    },
+    {
+      'product' : 'NVIDIA GeForce RTX 3080 10GB',
+      'url'     : 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
+    },
+    {
+      'product'   : 'MSI - NVIDIA GeForce RTX 3070 VENTUS 3X OC BV',
+      'url'       : 'https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3070-ventus-3x-oc-bv-8gb-gddr6-pci-express-4-0-graphics-card/6438278.p?skuId=6438278',
+    },
+    {
+      'product' : 'EVGA - GeForce RTX 3080 XC3 ULTRA GAMING 10GB',
+      'url'     : 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432400.p?skuId=6432400'
+    },
+    {
+      'product'   : 'EVGA - GeForce RTX 3070 XC3 ULTRA GAMING 8GB',
+      'url'       : 'https://www.bestbuy.com/site/evga-geforce-rtx-3070-xc3-ultra-gaming-8gb-gddr6-pci-express-4-0-graphics-card/6439299.p?skuId=6439299',
+    },
+    {
+      'product'   : 'ASUS - GeForce RTX 3080 10GB GDDR6X',
+      'url'       : 'https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?skuId=6432445',
+    },
+    {
+      'product'   : 'ASUS - NVIDIA GeForce TUF-RTX3070 8GB',
+      'url'       : 'https://www.bestbuy.com/site/asus-nvidia-geforce-tuf-rtx3070-8gb-gddr6-pci-express-4-0-graphics-card-black/6439128.p?skuId=6439128',
+    },
+    {
+      'product'   : 'GIGABYTE - NVIDIA GeForce RTX 3080 GAMING OC 10GB',
+      'url'       : 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-gaming-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6430620.p?skuId=6430620',
+    },
+    {
+      'product'   : 'GIGABYTE - NVIDIA GeForce RTX 3070 GAMING OC 8GB',
+      'url'       : 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3070-gaming-oc-8gb-gddr6-pci-express-4-0-graphics-card/6437909.p?skuId=6437909',
+    },
+    {
+      'product'   : 'MSI - NVIDIA Geforce RTX 3080 VENTUS 3X 10G OC BV',
+      'url'       : 'https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-bv-gddr6x-pci-express-4-0-graphic-card-black-silver/6430175.p?skuId=6430175',
+    },
+    {
+      'product'   : 'GIGABYTE - NVIDIA GeForce RTX 3070 VISION OC 8GB',
+      'url'       : 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3070-vision-oc-8gb-gddr6-pci-express-4-0-graphics-card/6439385.p?skuId=6439385',
+    },
+    {
+      'product'   : 'GIGABYTE - NVIDIA GeForce RTX 3080 VISION OC 10GB',
+      'url'       : 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-vision-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6436219.p?skuId=6436219',
+    },
+    {
+      'product'   : 'GIGABYTE - NVIDIA GeForce RTX 3070 AORUS MASTER 8GB',
+      'url'       : 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3070-aorus-master-8gb-gddr6-pci-express-4-0-graphics-card/6439384.p?skuId=6439384',
+    },
+    {
+      'product'   : 'EVGA - GeForce RTX 3080 FTW3 ULTRA GAMING 10GB',
+      'url'       : 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-ftw3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6436196.p?skuId=6436196',
+    },
+];
